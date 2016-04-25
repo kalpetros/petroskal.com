29e0fae5d5edb6dba49695f1cbc31e47b9c5755e
Petros Kalogiannakis <kalpetros@gmail.com>
2016-04-26 02:21:28 +0300

    feat: some UI changes

--- js/toggle.js
+++ js/toggle.js
@@ -1,25 +1,17 @@
-// Hide email & pgp
-$( ".email" ).hide();
-$( ".pgp" ).hide();
-
-// Show email on button click
-$("#email").click(function(){
-  $(".email").slideDown( "slow", function() {
-    $(".email").show();
-  });
-  // Hide email after 5 seconds
-  $(document).ready( function() {
+$(document).ready(function() {
+  // Show name & description
+  $('.cover-heading').hide();
+  $('.lead').hide();
+  $('.cover-heading').fadeIn();
+  $('.lead').fadeIn();
+  // Hide email
+  $(".email").hide();
+  // Show email on button click
+  $("#email").click(function(){
+    $(".email").slideDown( "slow", function() {
+      $(".email").show();
+    });
+    // Hide email after 5 seconds
     $('.email').delay(4000).slideUp();
-  });
-});
-
-// Show pgp on button click
-$("#pgp").click(function(){
-  $(".pgp").slideDown( "slow", function() {
-    $(".pgp").show();
-  });
-  // Hide pgp after 5 seconds
-  $(document).ready( function() {
-    $('.pgp').delay(4000).slideUp();
   });
 });
