e8149980638bef17ddc22ba6a79bf6be808f3b17
Petros Kalogiannakis <github@petroskal.com>
2020-12-05 22:14:17 +0200

    Remove underline class from ImageItem

--- src/pages/about.js
+++ src/pages/about.js
@@ -64,9 +64,12 @@ const ImageItem = props => {
       className="flex bg-gray-100 dark:bg-gray-700 rounded-full h-12 w-12 items-center justify-center relative has-tooltip"
       data-tooltip={name}
     >
-      <a className="underline" href={url} target="__blank">
+      <a href={url} target="__blank">
         {icon ? (
-          <FontAwesomeIcon className="text-gray-700 dark:text-gray-200" icon={props.icon} />
+          <FontAwesomeIcon
+            className="text-gray-700 dark:text-gray-200"
+            icon={props.icon}
+          />
         ) : image ? (
           <Img
             fluid={image}
