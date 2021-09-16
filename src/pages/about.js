d6c461441ee78a68c697139d85318021ef8c47ec
Petros Kalogiannakis <github@petroskal.com>
2021-09-16 11:54:11 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -89,10 +89,6 @@ const About = props => {
         be used with minimal effort.
       </p>
       <p className="text-gray-700 dark:text-gray-400">
-        I'm currently working with a semi-remote agile team in a travel
-        technology startup.
-      </p>
-      <p className="text-gray-700 dark:text-gray-400">
         When I'm away from the keyboard you will usually find me planning my
         next travel destination, creating music or picking up{" "}
         <Link to="/books" className="text-indigo-500" target="__blank">
